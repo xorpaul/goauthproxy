3a8344836311fd62c6d7c71634bda93e488f2468
Andreas Paul <xorpaul@gmail.com>
2022-10-26 18:22:03 +0200

    add Name, PassThrough, Proxy and UrlObject

--- goauthproxy.go
+++ goauthproxy.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strconv"
 	"time"
@@ -25,29 +26,29 @@ var buildversion string
 
 // configSettings contains the key value pairs from the config file
 type ConfigSettings struct {
-	Timeout                    time.Duration `yaml:"timeout"`
-	ListenAddress              string        `yaml:"listen_address"`
-	ListenPort                 int           `yaml:"listen_port"`
-	PrivateKey                 string        `yaml:"ssl_private_key"`
-	CertificateFile            string        `yaml:"ssl_certificate_file"`
-	RequireAndVerifyClientCert bool          `yaml:"ssl_require_and_verify_client_cert"`
-	ClientCertCaFile           string        `yaml:"ssl_client_cert_ca_file"`
-	LogBaseDir                 string        `yaml:"log_base_dir"`
-	RequestsTrustedRootCas     []string      `yaml:"requests_trusted_root_cas"`
-	Endpoints                  map[string]endpointSettings
+	Debug                  bool          `yaml:"debug"`
+	Timeout                time.Duration `yaml:"timeout"`
+	ListenAddress          string        `yaml:"listen_address"`
+	ListenPort             int           `yaml:"listen_port"`
+	PrivateKey             string        `yaml:"ssl_private_key"`
+	CertificateFile        string        `yaml:"ssl_certificate_file"`
+	ClientCertCaFile       string        `yaml:"ssl_client_cert_ca_file"`
+	LogBaseDir             string        `yaml:"log_base_dir"`
+	RequestsTrustedRootCas []string      `yaml:"requests_trusted_root_cas"`
+	Endpoints              map[string]endpointSettings
 }
 
 type endpointSettings struct {
-	AllowedIps []string          `yaml:"allowed_Ips"`
-	AllowedCns []string          `yaml:"allowed_cns"`
-	Url        string            `yaml:"url"`
-	Headers    map[string]string `yaml:"headers"`
-	PostData   string            `yaml:"post_data"`
-	HttpType   string            `yaml:"http_type"`
-}
-
-type header struct {
-	Value string
+	Name        string
+	AllowedIps  []string `yaml:"allowed_Ips"`
+	AllowedCns  []string `yaml:"allowed_cns"`
+	Url         string   `yaml:"url"`
+	UrlObject   *url.URL
+	Headers     map[string]string `yaml:"headers"`
+	PostData    string            `yaml:"post_data"`
+	HttpType    string            `yaml:"http_type"`
+	PassThrough bool              `yaml:"pass_through"`
+	Proxy       string            `yaml:"proxy"`
 }
 
 type HttpResult struct {
@@ -82,6 +83,10 @@ func main() {
 
 	h.Debugf("using config file " + *configFile)
 	config = readConfigfile(*configFile)
+	if config.Debug {
+		h.Debug = true
+		h.Debugf("DEBUG mode set in config file " + *configFile)
+	}
 
 	http.HandleFunc("/", httpHandler)
 
@@ -90,30 +95,27 @@ func main() {
 	//Use only TLS v1.2
 	tlsConfig.MinVersion = tls.VersionTLS12
 
-	if config.RequireAndVerifyClientCert {
+	//Expect and verify client certificate against a CA cert
+	tlsConfig.ClientAuth = tls.VerifyClientCertIfGiven
 
-		//Expect and verify client certificate against a CA cert
-		tlsConfig.ClientAuth = tls.RequireAndVerifyClientCert
-
-		if len(config.ClientCertCaFile) > 1 {
-			if !h.FileExists(config.ClientCertCaFile) {
-				h.Fatalf("could not find client CA file: " + config.ClientCertCaFile)
-			} else {
-				// Load CA cert
-				caCert, err := ioutil.ReadFile(config.ClientCertCaFile)
-				if err != nil {
-					h.Fatalf("Error while reading client certificate CA file " + config.ClientCertCaFile + " Error: " + err.Error())
-				}
-				caCertPool := x509.NewCertPool()
-				caCertPool.AppendCertsFromPEM(caCert)
-				tlsConfig.ClientCAs = caCertPool
-				h.Debugf("Expecting and verifying client certificate against " + config.ClientCertCaFile)
-			}
+	if len(config.ClientCertCaFile) > 1 {
+		if !h.FileExists(config.ClientCertCaFile) {
+			h.Fatalf("could not find client CA file: " + config.ClientCertCaFile)
 		} else {
-			h.Fatalf("A client certificate CA file needs to be specified if client certificate verification is enabled")
+			// Load CA cert
+			caCert, err := ioutil.ReadFile(config.ClientCertCaFile)
+			if err != nil {
+				h.Fatalf("Error while reading client certificate CA file " + config.ClientCertCaFile + " Error: " + err.Error())
+			}
+			caCertPool := x509.NewCertPool()
+			caCertPool.AppendCertsFromPEM(caCert)
+			tlsConfig.ClientCAs = caCertPool
+			h.Debugf("Expecting and verifying client certificate against " + config.ClientCertCaFile)
 		}
-
+	} else {
+		h.Fatalf("A client certificate CA file needs to be specified if client certificate verification is enabled")
 	}
+
 	server := &http.Server{
 		Addr:      config.ListenAddress + ":" + strconv.Itoa(config.ListenPort),
 		TLSConfig: tlsConfig,
